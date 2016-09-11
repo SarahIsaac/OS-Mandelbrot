7764d11b5ad8cd8321d92ae09a82e524adf1c193
SarahIsaac <sarahisaac31@gmail.com>
2016-09-10 18:23:32 -0600

    change back

--- Main.cpp
+++ Main.cpp
@@ -3,17 +3,7 @@
 #include <fstream>
 #include <vector>
 #include <chrono>
-
-template<typename F>
-double functionTimer(F f)
-{
-	#include<chrono>
-	std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
-	f();
-	std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-	std::chrono::milliseconds result = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-	return result.count();
-}
+#include "timer.h"
 
 struct Color {
 	int red;
